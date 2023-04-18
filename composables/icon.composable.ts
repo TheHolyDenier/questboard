dc43fda793c374856cd338c9943341dd7cc9a009
TheHolyDenier <bloodthirsty.snow.white@gmail.com>
2023-04-18 23:25:33 +0200

    Icons

--- composables/icon.composable.ts
+++ composables/icon.composable.ts
@@ -3,7 +3,28 @@ import { flatten } from 'lodash';
 export const useIcon = () => {
   const credits = [
     { author: 'Faithtoken', icons: ['card-discard'] },
-    { author: 'Delapouite', icons: ['card-joker', 'wood-cabin'] }
+    {
+      author: 'Delapouite',
+      icons: [
+        'card-joker',
+        'wood-cabin',
+        'barbarian',
+        'dwarf-face',
+        'female-vampire',
+        'vampire-dracula',
+        'fish-monster',
+        'goblin-head',
+        'goblin-camp',
+        'golem-head',
+        'ogre',
+        'orc-head',
+        'wizard-face',
+        'woman-elf-face'
+      ]
+    },
+    { author: 'Kier Heyl', icons: ['dwarf-helmet', 'elf-helmet'] },
+    { author: 'Skoll', icons: ['troll'] },
+    { author: 'Lorc', icons: ['diablo-skull', 'dragon-head', 'visored-helm'] }
   ];
 
   const icons = flatten(credits.map((credit) => credit.icons));
