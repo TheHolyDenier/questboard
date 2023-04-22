a751cda0616aae345ebaf3c8eeae6a378ac3069e
TheHolyDenier <bloodthirsty.snow.white@gmail.com>
2023-04-22 09:56:27 +0200

    Created dialog

--- components/DeleteButton.vue
+++ components/DeleteButton.vue
@@ -3,7 +3,8 @@ import BaseDialog from '~/components/base/BaseDialog.vue';
 
 const $emit = defineEmits<{ (e: 'on:delete'): void }>();
 
-const showDialog = ref(true);
+const showDialog = ref(false);
+
 const onDelete = () => {
   $emit('on:delete');
   showDialog.value = false;
@@ -16,22 +17,23 @@ const onDelete = () => {
     label="Delete"
     @click="showDialog = true"
   />
-
-  <BaseDialog
-    v-if="showDialog"
-    title="Are you sure you want to delete?"
-    @on:cancel="showDialog = false"
-  >
-    <template #default>This action cannot be undone.</template>
-    <template #actions>
-      <BaseButton label="Delete" @click="onDelete" />
-      <BaseButton
-        label="Cancel"
-        color="transparent"
-        @click="showDialog = false"
-      />
-    </template>
-  </BaseDialog>
+  <Transition>
+    <BaseDialog
+      v-if="showDialog"
+      title="Are you sure you want to delete?"
+      @on:cancel="showDialog = false"
+    >
+      <template #default>This action cannot be undone.</template>
+      <template #actions>
+        <BaseButton label="Delete" @click="onDelete" />
+        <BaseButton
+          label="Cancel"
+          color="transparent"
+          @click="showDialog = false"
+        />
+      </template>
+    </BaseDialog>
+  </Transition>
 </template>
 
 <style scoped lang="scss"></style>
