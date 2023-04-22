b75cede01ec33895e7d4c06397d7fdc8da4e63ab
TheHolyDenier <bloodthirsty.snow.white@gmail.com>
2023-04-22 23:11:08 +0200

    Updated backend

--- manager/elements.manager.ts
+++ manager/elements.manager.ts
@@ -1,24 +1,13 @@
-import { Element, User } from '@prisma/client';
+import { Element } from '@prisma/client';
 import { prisma } from '~/server/api';
-import { getStatusCode, StatusMessageEnum } from '~/enums/status-message.enum';
 
 export class ElementsManager {
-  static findOne = async (user: User, id: string) => {
-    const data = await prisma.element.findFirstOrThrow({
+  static findOne = async (id: string) => {
+    return prisma.element.findFirstOrThrow({
       where: {
-        id,
-        OR: [
-          { createdById: { equals: user.id } }
-          // { players: { some: { id: user.id } } }
-        ]
+        id
       }
     });
-
-    return {
-      statusCode: getStatusCode(StatusMessageEnum.OK),
-      statusMessage: StatusMessageEnum.OK,
-      data
-    };
   };
 
   static findMany = async (campaignId: string): Promise<Element[]> => {
--- server/api/secure/campaigns/[campaignId]/elements/[elementId]/index.ts
+++ server/api/secure/campaigns/[campaignId]/elements/[elementId]/index.ts
@@ -5,25 +5,45 @@ import { getStatusCode, StatusMessageEnum } from '~/enums/status-message.enum';
 import { prisma } from '~/server/api';
 import { notFoundError } from '~/errors/not-found.error';
 import { ElementsManager } from '~/manager/elements.manager';
+import { CampaignsManager } from '~/manager/campaigns.manager';
+import { ApiMethodsEnum } from '~/enums/api-methods.enum';
 
 export default defineEventHandler(
   async (event: H3Event): Promise<ApiResponse<Element | null>> => {
-    const user = event.context.user;
+    await CampaignsManager.getParamAndFind(event);
+
     const query = getQuery(event);
-
-    const body = await readBody(event);
-
-    const element = await ElementsManager.findOne(user, String(query.id));
-
+    const element = await ElementsManager.findOne(String(query.id));
     if (!element) throw notFoundError();
 
-    return {
-      statusCode: getStatusCode(StatusMessageEnum.OK),
-      statusMessage: StatusMessageEnum.OK,
-      data: await prisma.element.update({
-        where: { id: String(query.id) },
-        data: { ...body, updatedAt: new Date() }
-      })
-    };
+    switch (event.context.method) {
+      case ApiMethodsEnum.PATCH:
+        return updateElement(element.id, event);
+      case ApiMethodsEnum.GET:
+        return getElement(element);
+      default:
+        throw notFoundError();
+    }
   }
 );
+
+const getElement = async (element: Element) => {
+  return {
+    statusCode: getStatusCode(StatusMessageEnum.OK),
+    statusMessage: StatusMessageEnum.OK,
+    data: element
+  };
+};
+
+const updateElement = async (elementId: string, event: H3Event) => {
+  const body = await readBody(event);
+
+  return {
+    statusCode: getStatusCode(StatusMessageEnum.OK),
+    statusMessage: StatusMessageEnum.OK,
+    data: await prisma.element.update({
+      where: { id: elementId },
+      data: { ...body, updatedAt: new Date() }
+    })
+  };
+};
