c7ecdcc4b64601439495600e494122fee5099998
TheHolyDenier <bloodthirsty.snow.white@gmail.com>
2023-04-23 00:15:20 +0200

    Fixed elements

--- server/api/secure/elements/[elementId].patch.ts
+++ server/api/secure/elements/[elementId].patch.ts
@@ -1,4 +1,4 @@
-import { defineEventHandler, getQuery, H3Event, readBody } from 'h3';
+import { defineEventHandler, H3Event, readBody } from 'h3';
 import { Element } from '@prisma/client';
 import { ApiResponse } from '~/interfaces/api-response';
 import { getStatusCode, StatusMessageEnum } from '~/enums/status-message.enum';
@@ -11,8 +11,12 @@ export default defineEventHandler(
   async (event: H3Event): Promise<ApiResponse<Element>> => {
     await CampaignsManager.getParamAndFind(event);
 
-    const query = getQuery(event);
-    const element = await ElementsManager.findOne(String(query.id));
+    const elementId = getRouterParam(event, 'elementId');
+    const element = await ElementsManager.findOne(
+      event.context.user,
+      elementId
+    );
+
     if (!element) throw notFoundError();
 
     const body = await readBody(event);
